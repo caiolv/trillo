965d7a5fd019f6e449c01b83c3be91e325ff8549
Caio Oliveira <caiolv@me.com>
2022-04-06 16:31:42 -0300

    add: start header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
+import sprite from '../assets/sprite.svg';
+import logo from '../assets/logo.png';
+import user from '../assets/user.jpg';
+
 function Header() {
   return (
     <header className="header">
-        Header part
+        <img src={logo} alt="trillo logo" className="logo" />
+        <form action="#" className="search">
+          <input type="text" className="search__input" placeholder="Search hotels" />
+          <button className="search__button">
+            <svg className="search__icon">
+              <use href={sprite + "#icon-magnifying-glass"}></use>
+            </svg>
+          </button>
+        </form>
+        <nav className='user-nav'>
+            <div className="user-nav__icon-box">
+                <svg className='user-nav__icon'>
+                    <use href={sprite + "#icon-bookmark"}></use>
+                </svg>
+                <span className='user-nav__notification'>7</span>
+            </div>
+            <div className="user-nav__icon-box">
+                <svg className='user-nav__icon'>
+                    <use href={sprite + "#icon-chat"}></use>
+                </svg>
+                <span className='user-nav__notification'>13</span>
+            </div>
+            <div className='user-nav__user'>
+                <img src={user} alt="User Photo" className='user-nav__user-photo' />
+                <span className='user-nav__user-name'>Jonas</span>
+            </div>
+        </nav>
     </header>
   );
 }
